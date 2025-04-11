b3deb4dfbe6a157fbdfb574d539fd56e35894b5a
ok-avi <128667460+ok-avi@users.noreply.github.com>
2025-04-11 15:39:39 +0530

    fix: removed coreoptions

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 
-const coreOption = {
-    origin: "*",
-    Credential: true,
-};
+// const coreOption = {
+//     origin: "*",
+//     Credential: true,
+// };
 
 const app = express();
 initializeDB();
 app.use(express.json());
-app.use(cors(coreOption));
+app.use(cors());
 
 async function getAllHotels() {
     try {
